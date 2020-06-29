e990eac93b19027bde3dfdec68ddd6ae8e60d4f2
M Kumaran <veerammalkumaran@gmail.com>
2020-06-29 22:29:26 +0530

    init

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
