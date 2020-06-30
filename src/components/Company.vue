bdd03b3aa804d76fc515f99e8670864eb1e32a08
M Kumaran <veerammalkumaran@gmail.com>
2020-07-01 00:04:37 +0530

    socket connection created

---
+++ src/components/Company.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="company">
+    <div>{{ name }}</div>
+    <div>{{ stock }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Company",
+  props: {
+    name: String,
+    stock: Number,
+  },
+};
+</script>
+
+<style scoped>
+.company {
+  width: 70px;
+  height: 45px;
+  background-color: lightsteelblue;
+  margin: 2px;
+  padding: 2px;
+}
+</style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,17 +2,63 @@
   <div>
     <h1>Dashboard</h1>
     <hr />
+    <div class="companyBox">
+      <Company v-for="company in companies" :key="company.id" v-bind:name="company.name" v-bind:stock="company.stock" />
+    </div>
   </div>
 </template>
 
 <script>
+import Company from "./Company";
+import SocketIOClient from "socket.io-client";
+
+const socket = SocketIOClient.connect("http://localhost:3001");
+
 export default {
   name: "Dashboard",
+  components: {
+    Company,
+  },
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      companies: [],
+    };
+  },
+  beforeMount() {
+    this.getName();
+  },
+  created() {
+    socket.on("newStockValues", (data) => {
+      const companiesLatestUpdated = this.companies.map((comp) => {
+        const dataForThisComp = data.find((d) => d.id === comp.id);
+        if (dataForThisComp) {
+          return dataForThisComp;
+        } else {
+          return comp;
+        }
+      });
+      this.companies = companiesLatestUpdated;
+    });
+  },
+  beforeDestroy() {
+    //this.sockets.unsubscribe("newStockValues");
+  },
+  methods: {
+    async getName() {
+      const res = await fetch("http://localhost:3001/companies");
+      const data = await res.json();
+      this.companies = data;
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.companyBox {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
